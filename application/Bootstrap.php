8d7f2eac8cbd4e7a3cc5f6faf9196638c8e4b045
jmgm <jasiej@gmail.com>
2012-08-20 00:41:42 +0200

    Config update

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,7 +8,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 	
 	protected function _initDB() {
 		//Globalny dostêp do bazy danych
-		$dbConfig = new Zend_Config_Ini('../application/local_database_config.ini', 'database_config');
+		$dbConfig = new Zend_Config_Ini('../application/local_db_config.ini', 'database_config');
 		Zend_Registry::set('db', Zend_Db::factory($dbConfig -> database));
 	}
 
