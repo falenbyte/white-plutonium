bbdd70f36f12f56cef81a5c446a5ae7d7477d797
jmgm <jasiej@gmail.com>
2012-08-19 20:01:04 +0200

    SearchFilters and Category update

--- application/models/Category.php
+++ application/models/Category.php
@@ -5,5 +5,10 @@ class Application_Model_Category extends Application_Model_AbstractDataStorage {
 	protected function initVariableList() {
 		$this -> variablesList = array('ID', 'parentID', 'name');
 	}
+	
+	public function getAttributes() {
+		$mapper = new Application_Model_AttributesMapper();
+		return $mapper -> getByCategory($this -> data['ID']);
+	}
 
 }
--- application/models/SearchFilters.php
+++ application/models/SearchFilters.php
@@ -1,8 +1,71 @@
 <?php
 
+// TODO: konstruowanie na podstawie formularza wyszukiwania
+
 class Application_Model_SearchFilters
-{
+	{
+		private $_filters;
+		
+		public function __construct()
+			{
+				$_filters = array();
+			}
+		
+		public function addFilter($type, $value)
+			{
+				if($type != 'catID'
+					&& $type != 'userID'
+					&& $type != 'attribute'
+					&& $type != 'keywords')
+					throw new Exception('Unknown filter type: ' . $type);
+				
+				$fun = '_add' . ucfirst($type);
+				$this->$fun($value);
+			}
+		
+		private function _addCatID($value)
+			{
+				if(preg_match('/[0-9]+/', $value))
+					$this->_filters['catID'] = $value;
+				else
+					throw new Exception('Supplied category ID is invalid.');	
+			}
+		
+		private function _addUserID($value)
+			{
+				if(preg_match('/[0-9]+/', $value))
+					$this->_filters['userID'] = $value;
+				else
+					throw new Exception('Supplied user ID is invalid.');
+			}
+		
+		private function _addAttribute($value)
+			{
+				if(!is_array($value))
+					throw new Exception('The value for attribute filter must be an array.');
+				
+				if(!isset($value['ID']) || !preg_match('/[0-9]+/', $value['ID']))
+					throw new Exception('Attribute ID not supplied or invalid.');
+				
+				if(!isset($value['values']) || !is_array($value['values']))
+					throw new Exception('Attribute values not supplied or invalid.');
+				
+				$mapper = new Application_Model_AttributesMapper();
+				$att = $mapper->getByID($value['ID']);
+				
+				foreach($value['values'] as $attValue)
+				{
+					if(!$att->validateValue($attValue))
+						throw new Exception('Attribute value invalid: ' . $attValue);
+				}
+				
+				$this->_filters['attributes'][$value['ID']] = $value['values'];
+			}
+			
+		private function _addKeywords($value)
+			{
+				$this->filters['keywords'] = $value;
+			}
+	}
 
-
-}
-
+// addFilter( 'attribute', array('ID'=>'5', 'values'=>array('50', '80')) );
