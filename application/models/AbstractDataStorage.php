ad5c0cb39b33b8b0340c615f2deb5fbba4c11382
falenbyte <falenbyte+github@gmail.com>
2012-08-19 22:05:21 +0200

    Revoked AbstractDataStorage update

--- application/models/AbstractDataStorage.php
+++ application/models/AbstractDataStorage.php
@@ -6,25 +6,15 @@ abstract class Application_Model_AbstractDataStorage {
 
 	protected $data;
 	protected $variablesList;
-	private $readAccessList;
-	private $writeAccessList;
 	
 	final public function __construct($initArray = false) {
 		$this -> data = array();
+		$this -> variablesList = array();
 		$this -> initVariableList();
-		$init = is_array($initArray);
-		foreach($this -> variablesList as $variable) {
-			$variable = explode('|', $variable, 2);
-			if(!isset($variable[1])) {
-				$variable[1] = 'rw';
+		if(is_array($initArray)) {
+			foreach($this -> variablesList as $variable) {
+				$this -> data[$variable] = $initArray[$variable];
 			}
-			if(strpos($variable[1], 'r') !== false) {
-				$this -> readAccessList[] = $variable[0];
-			}
-			if(strpos($variable[1], 'w') !== false) {
-				$this -> writeAccessList[] = $variable[0];
-			}
-			if($init) $this -> data[$variable[0]] = $initArray[$variable[0]];
 		}
 	}
 	
@@ -32,7 +22,7 @@ abstract class Application_Model_AbstractDataStorage {
 	abstract protected function initVariableList();
 	
 	final public function __get($name) {
-		if(in_array($name, $this -> readAccessList)) {
+		if(in_array($name, $this -> variablesList)) {
 			return $this -> data[$name];
 		} else {
 			throw new Exception('Trying to read wrong variable.');
@@ -40,7 +30,7 @@ abstract class Application_Model_AbstractDataStorage {
 	}
 	
 	final public function __set($name, $value) {
-		if(in_array($name, $this -> writeAccessList)) {
+		if(in_array($name, $this -> variablesList)) {
 			$this -> data[$name] = $value;
 		} else {
 			throw new Exception('Trying to write to wrong variable.');
