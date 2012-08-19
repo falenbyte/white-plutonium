8586051e7d237ec805d6476e91e0bf209da1c34c
falenbyte <falenbyte+github@gmail.com>
2012-08-19 20:03:54 +0200

    Attribute models update

--- application/models/Attribute.php
+++ application/models/Attribute.php
@@ -6,9 +6,28 @@ class Application_Model_Attribute extends Application_Model_AbstractDataStorage 
 		$this -> variablesList = array('ID', 'name', 'type', 'options', 'unit', 'min', 'max');
 	}
 	
-	public function addOption($option) {
-		if(preg_match('/[a-zA-Z0-9\(\), ]+/', $option)) {
-			$this -> data['options'][] = $option;
+	public function addOption($id, $name) {
+		if(preg_match('/[0-9]+/', $id)) {
+			if(preg_match('/[a-zA-Z0-9\(\), ]+/', $option)) {
+				$this -> data['options'][$id] = $option;
+			} else {
+				throw new Exception('Trying to add option with wrong name.');
+			}
+		} else {
+			throw new Exception('Trying to add option with wrong ID.');
+		}
+	}
+	
+	public function validateValue($value) {
+		switch($this -> data['type']) {
+			case 0:
+				return (is_integer($value) && $value >= $this -> data['min'] && $value <= $this -> data['max']);
+			case 1:
+				return is_string($value);
+			case 2:
+				return array_key_exists($calue, $this -> data['options']);
+			case 3:
+				return ($value === 0 || $value === 1);
 		}
 	}
 
--- application/models/AttributesMapper.php
+++ application/models/AttributesMapper.php
@@ -8,7 +8,24 @@ class Application_Model_AttributesMapper {
 		$this -> db = Zend_Registry::get('db');
 	}
 	
-	public function getByCategory($catID) {
+	public function getAll() {
+		$result = $this -> db -> fetchAssoc('SELECT * FROM attributes');
+		foreach($result as $row) {
+			$attributes[$row['ID']] = new Application_Model_Attribute($row);
+			if($row['type'] == 2) {
+				$optionsList[] = $row['ID'];
+			}
+		}
+		if(isset($optionsList)) {
+			$options = $this -> db -> fetchAssoc('SELECT * FROM attributes_options WHERE attID IN (?)', implode(',', $optionsList));
+			foreach($options as $option) {
+				$attributes[$option['attID']] -> addOption($option['ID'], $option['name']);
+			}
+		}
+		return $attributes;
+	}
+	
+	public function getByCategoryID($catID) {
 		if(preg_match('/[0-9]+/', $catID)) {
 			$attributesList = $this -> db -> fetchNum('SELECT attID FROM categories_attributes WHERE catID = ?', $catID);
 			$result = $this -> db -> fetchAssoc('SELECT * FROM attributes WHERE ID IN (?)', implode(',', $attributesList));
@@ -18,11 +35,29 @@ class Application_Model_AttributesMapper {
 					$optionsList[] = $row['ID'];
 				}
 			}
-			$options = $this -> db -> fetchAssoc('SELECT * FROM attributes_options WHERE ID IN (?)', implode(',', $optionsList));
-			foreach($options as $option) {
-				$attributes[$option['attID']] -> addOption($option['option']);
+			if(isset($optionsList)) {
+				$options = $this -> db -> fetchAssoc('SELECT * FROM attributes_options WHERE attID IN (?)', implode(',', $optionsList));
+				foreach($options as $option) {
+					$attributes[$option['attID']] -> addOption($option['ID'], $option['name']);
+				}
 			}
 			return $attributes;
+		} else {
+			throw new Exception('Supplied category ID is invalid.');
+		}
+	}
+	
+	public function getByID($attID) {
+		if(preg_match('/[0-9]+/', $catID)) {
+			$result = $this -> db -> fetchAssoc('SELECT * FROM attributes WHERE ID = ?', $attID);
+			$attribute = new Application_Model_Attribute($result);
+			if($row['type'] == 2) {
+					$options = $this -> db -> fetchAssoc('SELECT * FROM attributes_options WHERE attID = ?', $attID);
+					foreach($options as $option) {
+						$attribute[$option['attID']] -> addOption($option['ID'], $option['name']);
+					}
+			}
+			return $attribute;
 		} else {
 			throw new Exception('Supplied category ID is invalid.');
 		}
