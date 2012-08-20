c0c7e84dbadba7cff732ba823a91b2105fd89511
falenbyte <falenbyte+github@gmail.com>
2012-08-20 17:03:12 +0200

    User update

--- application/models/User.php
+++ application/models/User.php
@@ -3,27 +3,39 @@
 class Application_Model_User {
 
 	private $userData;
+	private $session;
 	
 	public function __construct() {
-		
+		$this -> session = new Zend_Session_Namespace('UserData');
+		if(!isset($this -> session -> authenticated)) {
+			$this -> session -> authenticated = false;	
+		}
 	}
 	
 	public function login($username, $password) {
+		if($this -> session -> auth) { //Je¿eli zalogowany nie tworzymy konta
+			throw new Exception('User is already logged in');
+		}
 		if(!preg_match('/[a-zA-Z0-9_]+/', $username)) {
 			throw new Exception('Username contains forbidden characters');
 		}
 		$db = Zend_Registry::get('db');
 		$userData = $db -> fetchRow('SELECT * FROM users WHERE username = ?', $username);
 		if($userData === false || $userData['password'] != md5($password.$userData['salt'])) {
+			$this -> session -> auth = false;
 			return false;
+			
 		} else {
-			$this -> userData = $userData;
+			$this -> session -> auth = true;
+			$this -> session -> userID = $userData['ID'];
+			$this -> session -> username = $userData['username'];
+			$this -> session -> setExpirationSeconds(60 * 60 * 24 * 10);
 			return true;
 		}
 	}
 	
 	public function logout() {
-		
+		$this -> session -> auth = false;
 	}
 	
 	private function generateSalt() {
@@ -32,11 +44,14 @@ class Application_Model_User {
 	}
 	
 	public function register($username, $password, $email) {
+		if($this -> session -> auth) { //Je¿eli zalogowany nie tworzymy konta
+			throw new Exception('User is logged in');
+		}
 		if(!preg_match('/[a-zA-Z0-9_]+/', $username)) {
 			throw new Exception('Username contains forbidden characters.');
 		}
 		// TODO: poprawiæ wyra¿enie regularne
-		if(!preg_match('/[a-zA-Z0-9_]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/', $email)) {
+		if(!preg_match('/[a-zA-Z0-9_-\.\+]+@[a-zA-Z0-9\.]+\.[a-zA-Z]{2,}/', $email)) {
 			throw new Exception('Supplied email is invalid.');
 		}
 		$db = Zend_Registry::get('db');
@@ -46,11 +61,14 @@ class Application_Model_User {
 	}
 	
 	public function isLoggedIn() {
-		return isset($this -> userData['ID']);
+		return $this -> session -> auth;
 	}
 	
 	public function getUserID() {
-		return isset($this -> userData['ID']) ? $this -> userData['ID'] : false;
+		return $this -> session -> auth ? $this -> session -> userID : false;
 	}
-
+	
+	public function getUserName() {
+		return $this -> session -> auth ? $this -> session -> username : false;
+	}
 }
