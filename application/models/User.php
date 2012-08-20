5c1c645c2d4ed208ef7668c6950cd1ea863688dc
falenbyte <falenbyte+github@gmail.com>
2012-08-20 19:37:53 +0200

    User update

--- application/models/User.php
+++ application/models/User.php
@@ -4,19 +4,19 @@ class Application_Model_User {
 
 	private $session;
 	private $db;
-	
+
 	public function __construct() {
 		$this -> db = Zend_Registry::get('db');
 		//Sesje!!
 		$this -> session = new Zend_Session_Namespace('UserData');
 		if(!isset($this -> session -> auth)) {
-			$this -> session -> auth = false;	
+			$this -> session -> auth = false;
 		}
 		if($this -> session -> auth) {
 			$this -> updateLastSeen();
 		}
 	}
-	
+
 	public function login($username, $password) {
 		if($this -> session -> auth) { //Je¿eli juz zalogowany to po co znowu logowac?
 			throw new Exception('User is already logged in');
@@ -25,7 +25,7 @@ class Application_Model_User {
 			throw new Exception('Username contains forbidden characters');
 		}
 		$userData = $this -> db -> fetchRow('SELECT * FROM users WHERE username = ?', $username, Zend_Db::FETCH_ASSOC);
-		if($userData === false || $userData['password'] != md5($password.$userData['salt'])) {
+		if($userData === false || $userData['password'] != $this -> makePasswordHash($password, $userData['salt'])) {
 			$this -> session -> auth = false;
 			return false;
 			
@@ -38,20 +38,24 @@ class Application_Model_User {
 			return true;
 		}
 	}
-	
+
 	public function logout() {
 		$this -> session -> auth = false;
 	}
-	
+
 	public function updateLastSeen() {
 		$this -> db -> update('users', array('last_seen' => time()), 'ID = ' . $this -> session -> userID);
+	}
+
+	private function makePasswordHash($password, $salt) {
+		return md5($password.$salt);
 	}
 	
 	private function generateSalt() {
 		mt_srand(microtime(true) * 10000);
 		return md5(mt_rand(0, mt_getrandmax()) * 51539607551); //Bardzo losowo wybrana przezemnie liczba pierwsza
 	}
-	
+
 	public function register($username, $password, $email) {
 		if($this -> session -> auth) { //Je¿eli zalogowany nie tworzymy konta
 			throw new Exception('User is logged in');
@@ -77,16 +81,58 @@ class Application_Model_User {
 		$queryData = array($username, md5($password.$salt), $salt, $email, time(), time());
 		$this -> db -> query('INSERT INTO users VALUES(null, ?, ?, ?, ?, ?, ?)', $queryData);
 	}
-	
+
+	public function changePassword($oldPassword, $newPassword) {
+		if(!$this -> session -> auth) {
+			throw new Exception('User is not logged in.');
+		}
+		$userData = $this -> db -> fetchRow('SELECT * FROM users WHERE ID = ?', $this -> session -> userID, Zend_Db::FETCH_ASSOC);
+		if($this -> makePasswordHash($oldPassword, $userData['salt']) == $userData['password']) {
+			$this -> db -> update('users', array('password' => $this -> makePasswordHash($newPassword, $userData['salt'])), 'ID = ' . $this -> session -> userID);
+		} else {
+			throw new Exception('Supplied password does not match the one in database.');
+		}
+	}
+
+	public function changeLostPassword($key, $newPassword) {
+		if(!preg_match('/[a-zA-Z0-9]{32}/', $key)) {
+			throw new Exception('Supplied key is invalid.');
+		}
+		$keyData = $this -> db -> fetchRow('SELECT * FROM lost_password_keys WHERE key = ?', $key, Zend_Db::FETCH_ASSOC);
+		if($keyData === false) {
+			throw new Exception('Supplied key is not in database');
+		}
+		if($keyData['expires'] < time()) {
+			throw new Exception('Supplied key expired.');
+		}
+		$userData = $this -> db -> fetchRow('SELECT * FROM users WHERE ID = ?', $keyData['userID'], Zend_Db::FETCH_ASSOC);
+		$this -> db -> update('users', array('password' => $this -> makePasswordHash($newPassword, $userData['salt'])), 'ID = ' . $keyData['userID']);
+	}
+
+	public function requestLostPasswordKey($username) {
+		if(!preg_match('/[a-zA-Z0-9_]+/', $username)) {
+			throw new Exception('Username contains forbidden characters.');
+		}
+		$userData = $this -> db -> fetchRow('SELECT * FROM users WHERE username = ?', $username, Zend_Db::FETCH_ASSOC);
+		if($userData === false) {
+			throw new Exception('No such username in database.');
+		}
+		$key = $this -> generateSalt();
+		$queryData = array($userData['ID'], $key, time() + (60 * 60 * 24));
+		$this -> db -> query('INSERT INTO lost_password_keys VALUES(null, ?, ?, ?)', $queryData);
+		return $key;
+	}
+
 	public function isLoggedIn() {
 		return $this -> session -> auth;
 	}
-	
+
 	public function getUserID() {
 		return $this -> session -> auth ? $this -> session -> userID : false;
 	}
-	
-	public function getUserName() {
+
+	public function getUsername() {
 		return $this -> session -> auth ? $this -> session -> username : false;
 	}
+
 }
