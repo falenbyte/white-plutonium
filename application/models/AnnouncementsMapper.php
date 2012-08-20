801a5d92338bdd0076808d4b7357d16d4b492de3
jmgm <jasiej@gmail.com>
2012-08-20 16:47:18 +0200

    AnnouncementsMapper update

--- application/models/AnnouncementsMapper.php
+++ application/models/AnnouncementsMapper.php
@@ -29,5 +29,42 @@ class Application_Model_AnnouncementsMapper
 				
 				return $ann;
 			}
+		
+		public function getByFilters(Application_Model_SearchFilters $filters)
+			{
+				$result = $this->_db->fetchAll($filters->getQueryString(), Zend_Db::FETCH_ASSOC);
+				
+				$attMapper = new Application_Model_AttributesMapper();
+				
+				foreach($result as $row)
+				{
+					$ann = new Application_Model_Announcement($row);
+					
+					$attDefs = $attMapper->getByCategoryID($ann->catID);
+					$attValues = $this->_db->fetchAll('SELECT attID, intValue, textValue, floatValue FROM attributes_values WHERE annID = ?',
+						$ann->ID, Zend_Db::FETCH_ASSOC);
+					
+					foreach($attValues as $att)
+						$ann->attributes[$att['attID']] = $att[$attDefs[$att['attID']]->getTypeString() . 'Value'];
+					
+					$ann->images = $this->_db->fetchPairs('SELECT images.ID, images.name FROM images ' .
+						'JOIN announcement_images ON (images.ID = announcement_images.imgID) WHERE announcement_images.annID = ?',
+						$ann->ID);
+					
+					$anns[] = $ann;
+				}
+				
+				return $anns;
+			}
+			
+		public function save(Application_Model_Announcement $announcement)
+			{
+				
+			}
+		
+		public function delete($id)
+			{
+				
+			}
 	}
 
