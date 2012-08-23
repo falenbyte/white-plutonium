87de211fb177532645e4ffc4a572e4163bfb20f0
jmgm <jasiej@gmail.com>
2012-08-23 22:47:21 +0200

    Update

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+function basicSearch()
+	{
+		document.getElementById('special_search_form').style.display = 'none';
+		document.getElementById('basic_search_form').style.display = 'block';
+	}
+
+function specialSearch()
+	{
+		document.getElementById('basic_search_form').style.display = 'none';
+		document.getElementById('special_search_form').style.display = 'block';
+	}
