e9baddb2ccf777483a89590ef7bab4cff1994bbd
jmgm <jasiej@gmail.com>
2012-08-26 04:19:48 +0200

    Editing

--- application/controllers/AnnouncementController.php
+++ application/controllers/AnnouncementController.php
@@ -111,9 +111,11 @@ class AnnouncementController extends Zend_Controller_Action {
 						{
 							$uploaded = $imagesModel->saveImages($_FILES['uploaded']['tmp_name'], $_FILES['uploaded']['size'],
 								$this->view->baseUrl('imgs/'));
+							
+							$images = array_merge($images, $uploaded);
 						}
 						
-						$images = array_merge($images, $uploaded);
+						$this->view->images = $images;
 					}
 					else
 					{
@@ -149,9 +151,129 @@ class AnnouncementController extends Zend_Controller_Action {
 			}
 		}
     
-    public function editAction() {
-    	
-    }
+    public function editAction()
+		{
+			$catMapper = new Application_Model_CategoriesMapper();
+			$attMapper = new Application_Model_AttributesMapper();
+			$annMapper = new Application_Model_AnnouncementsMapper();
+			$user = Zend_Registry::get('userModel');
+			
+			if(isset($_GET['id']) && preg_match('/^[0-9]+$/', $_GET['id']))
+				$ann = $annMapper->getByID($_GET['id']);
+			else if(isset($_POST['id']) && preg_match('/^[0-9]+$/', $_POST['id']))
+				$ann = $annMapper->getByID($_POST['id']);
+			else
+				$this->_redirect('index');
+			
+			if(!$user->isLoggedIn())
+				$this->_redirect('account');
+			else if(isset($ann) && $user->getUserID() == $ann->userID)
+			{
+				$messages = array();
+				$attDefs = $attMapper->getByCategoryID($ann->catID);
+				
+				$valid = true;
+				
+				$title = isset($_POST['title']) ? $_POST['title'] : $ann->title;
+				if($title == '')
+				{
+					$valid = false;
+					$messages[] = 'Musisz podać tytuł ogłoszenia.';
+				}
+				
+				$content = isset($_POST['content']) ? $_POST['content'] : $ann->content;
+				if($content == '')
+				{
+					$valid = false;
+					$messages[] = 'Musisz podać treść ogłoszenia.';
+				}
+				
+				foreach($attDefs as $aid => $aval)
+				{
+					if(isset($_POST[$aid]))
+						$attributes[$aid] = $_POST[$aid];
+					else if(isset($ann->attributes[$aid]) && !isset($_POST['done']))
+						$attributes[$aid] = $ann->attributes[$aid];
+					
+					if(isset($attributes[$aid]) && $attributes[$aid] != '' && !$attDefs[$aid]->validateValue($attributes[$aid]))
+					{
+						$valid = false;
+						$messages[] = 'Niepoprawna wartość atrybutu "' . strtolower($attDefs[$aid]->name) . '".';
+					}
+				}
+				
+				if(!$valid || !isset($_POST['done']))
+				{
+					$this->view->stage = 1; // ustawianie treści ogłoszenia
+					$this->view->category = $catMapper->getByID($ann->catID);
+					$this->view->attributes = $attDefs;
+					$this->view->messages = $messages;
+					$this->view->annID = $ann->ID;
+					$this->view->annTitle = $title;
+					$this->view->annContent = $content;
+					$this->view->annAttributes = $attributes;
+				}
+				else
+				{
+					if(!isset($_POST['finish']))
+					{
+						$this->view->stage = 2; // dodawanie obrazków
+						$this->view->annID = $ann->ID;
+						
+						$images = (isset($_POST['images']) ? $_POST['images'] : $ann->images);
+						
+						$imagesModel = new Application_Model_Images();
+						
+						if(isset($_POST['images_to_delete']))
+						{
+							$imagesModel->deleteImages($_POST['images_to_delete'], $this->view->baseUrl('imgs/'));
+							
+							foreach($images as $id => $name)
+							{
+								if(in_array($id, $_POST['images_to_delete']))
+									unset($images[$id]);
+							}
+						}
+						
+						if(is_array($_FILES['uploaded']))
+						{
+							$uploaded = $imagesModel->saveImages($_FILES['uploaded']['tmp_name'], $_FILES['uploaded']['size'],
+								$this->view->baseUrl('imgs/'));
+							
+							$images = array_merge($images, $uploaded);
+						}
+						
+						$this->view->images = $images;
+					}
+					else
+					{
+						$this->view->stage = 3; // koniec, sukces, wyświetl link do ogłoszenia
+						$this->view->annID = $ann->ID;
+						try
+						{
+							$ann->catID = $_POST['catID'];
+							$ann->title = $_POST['title'];
+							$ann->content = $_POST['content'];
+							$ann->images = (isset($_POST['images']) ? $_POST['images'] : array());
+							
+							foreach($_POST as $key => $value)
+							{
+								if(preg_match('/^[0-9]+$/', $key))
+									$atts[$key] = $value;
+							}
+							
+							$ann->attributes = $atts;
+							$annMapper->save($ann);
+						}
+						catch(Exception $e)
+						{
+							$this->view->stage = 4; //nieudany zapis, zmieniamy stage
+							$this->view->message = $e->getMessage();
+						}
+					}
+				}
+			}
+		}
     
     public function deleteAction() {
     	try {
